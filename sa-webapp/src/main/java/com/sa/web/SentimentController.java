659fad2500cd6376a4cc70158d319c009890f989
Shagun Bandi <shabandi@publicisgroupe.net>
2019-12-18 11:13:28 +0530

    Changes

--- sa-webapp/src/main/java/com/sa/web/SentimentController.java
+++ sa-webapp/src/main/java/com/sa/web/SentimentController.java
@@ -34,24 +34,21 @@ public class SentimentController {
     private final Logger log = LoggerFactory.getLogger(SentimentController.class);
         
     /**
-    * POST bank1
-    */
-    @PostMapping("/bank-1")
-    public String bank1(@RequestBody Map<String,String> payload) {
-        String card = payload.get("card");
-        if(card.equalsIgnoreCase("CC"))
-        {
-                String url = "http://35.193.37.248/api/payment/network";
-                HttpHeaders headers = new HttpHeaders();
-                headers.setContentType(MediaType.APPLICATION_JSON);
-                headers.set("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik5VSTFNRVpHT1RreE5VRXpOalZDTUVSR1FqRkRPVUZDUmtOQk1qUXpOekV4TlVaQlFrWTVPUSJ9.eyJpc3MiOiJodHRwczovL2Rldi05cWxidXBuMy5hdXRoMC5jb20vIiwic3ViIjoiWjlSVmg0Y2NBdTFWUkFFR2pHQlNJZExPeWs1OHQ0cGJAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vc2VudGltZW50LWFuYWx5c2lzLmlvIiwiaWF0IjoxNTc2NTY0MjM0LCJleHAiOjE1NzY2NTA2MzQsImF6cCI6Ilo5UlZoNGNjQXUxVlJBRUdqR0JTSWRMT3lrNTh0NHBiIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.d5VELxj4PyNIEAy4c-WWayEG0V-i4ZCWCCzBGXgCXRiJlrvVcW816F7J2BbzL9Kx7mu5wzXM9PH0FmGrqTjmXvksr1IuHkXOKw26gPpjeCMjU3ySFZAVS3tz0KCqaGxkz0rH78AjuSEZFBtQHs0Gi3HMy352ZkgvtgPbASBkVwIL4ejRIwUhkeLckbMvlOAvqKGL7KatGkRXfoeRd9uUVDVnwhzQxey_Soj0cCw2gqC2Fw2EAbNC5YUVWJHV7dt4nyqPb814HlJ4ijzfXyLFMhOa-1cn25701l8uCQfb_Eu6oR7IX4moBq-ogPZXf_rGJz6s8LFiuzrHNFQoDjL_aA");
-                Map<String,String> map=new HashMap<String, String>();
-                map.put("status","accept");
-                HttpEntity<Map<String, String>> request = new HttpEntity<>(map, headers);
-                RestTemplate restTemplate=new RestTemplate();
-                ResponseEntity<String> response = restTemplate.postForEntity(url, request , String.class);
-                return "Response from network: ("+response.getBody() + ")";
-        }
-        return "Not a valid card";
+     * PlatformResource controller
+     */
+    @PostMapping("/payment")
+    public String payment(@RequestBody Map<String,String> payload) {
+        String card = payload.get("cardType");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik5VSTFNRVpHT1RreE5VRXpOalZDTUVSR1FqRkRPVUZDUmtOQk1qUXpOekV4TlVaQlFrWTVPUSJ9.eyJpc3MiOiJodHRwczovL2Rldi05cWxidXBuMy5hdXRoMC5jb20vIiwic3ViIjoiWjlSVmg0Y2NBdTFWUkFFR2pHQlNJZExPeWs1OHQ0cGJAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vc2VudGltZW50LWFuYWx5c2lzLmlvIiwiaWF0IjoxNTc2NTY0MjM0LCJleHAiOjE1NzY2NTA2MzQsImF6cCI6Ilo5UlZoNGNjQXUxVlJBRUdqR0JTSWRMT3lrNTh0NHBiIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.d5VELxj4PyNIEAy4c-WWayEG0V-i4ZCWCCzBGXgCXRiJlrvVcW816F7J2BbzL9Kx7mu5wzXM9PH0FmGrqTjmXvksr1IuHkXOKw26gPpjeCMjU3ySFZAVS3tz0KCqaGxkz0rH78AjuSEZFBtQHs0Gi3HMy352ZkgvtgPbASBkVwIL4ejRIwUhkeLckbMvlOAvqKGL7KatGkRXfoeRd9uUVDVnwhzQxey_Soj0cCw2gqC2Fw2EAbNC5YUVWJHV7dt4nyqPb814HlJ4ijzfXyLFMhOa-1cn25701l8uCQfb_Eu6oR7IX4moBq-ogPZXf_rGJz6s8LFiuzrHNFQoDjL_aA");
+        Map<String,String> map=new HashMap<String, String>();
+        map.put("card",card);
+        String url = "http://35.239.195.224/api/payment/bank-1";
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(map, headers);
+        RestTemplate restTemplate=new RestTemplate();
+        ResponseEntity<String> response = restTemplate.postForEntity(url, request , String.class);
+        return "Response from acquiring bank: ( "+response.getBody() + " )";
     }
+
 }
